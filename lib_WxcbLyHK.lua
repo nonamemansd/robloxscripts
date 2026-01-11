b57f30c4bbcc1f03b1576b203cfc6d1ae3b2c9b2
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 17:26:50 +0200

    Push lib_WxcbLyHK.lua

---
+++ lib_WxcbLyHK.lua
@@ -0,0 +1,42 @@
+
+--[[ ENCRYPTED_ID: fc8e2ad8431542788bdeeca09e59037e ]]
+local tNpQV = 676455 + 78; -- // integrity_bd450
+local oVEwVMlTU = 766268 + 51; -- // integrity_1a0dd
+local ZJjScEKL = 21856 + 82; -- // integrity_ad083
+local OXdhNUFIKSDR = 369480 + 69; -- // integrity_40847
+local EbMKenSGX = 811003 + 80; -- // integrity_290fa
+local YLXIobGoOtT = 310481 + 40; -- // integrity_155fa
+local wGsCeCS = 389422 + 49; -- // integrity_f96fa
+local LImANUmcwaQY = 331851 + 43; -- // integrity_7e027
+local JVQsiHW = 257478 + 18; -- // integrity_9c027
+local JbyPIU = 880274 + 14; -- // integrity_fd03f
+local zswex = 665232 + 61; -- // integrity_7b03e
+local NnnCkp = 706445 + 96; -- // integrity_2753d
+local OaCFrNP = 210018 + 60; -- // integrity_193bd
+local txNKdPi = 494175 + 6; -- // integrity_d9e80
+local CttDUwjrgT = 629602 + 3; -- // integrity_9874e
+local CjFgBcX = 666750 + 44; -- // integrity_8da5d
+local EicSbdHcT = 130457 + 67; -- // integrity_0c7a9
+local CyeJK = 841146 + 38; -- // integrity_ddc10
+
+local uynKgbY = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local SOEJsPsYJe = Instance.new("Frame", uynKgbY)
+SOEJsPsYJe.Size = UDim2.new(0, 320, 0, 190)
+SOEJsPsYJe.Position = UDim2.new(0.5, -160, 0.5, -95)
+SOEJsPsYJe.BackgroundColor3 = Color3.fromRGB(16, 15, 32)
+Instance.new("UICorner", SOEJsPsYJe)
+local QtXAxPbAcPFOj = Instance.new("TextLabel", SOEJsPsYJe)
+QtXAxPbAcPFOj.Size = UDim2.new(1, 0, 0, 50)
+QtXAxPbAcPFOj.Text = "SOLAR UI"
+QtXAxPbAcPFOj.TextColor3 = Color3.new(1,1,1)
+QtXAxPbAcPFOj.BackgroundTransparency = 1
+local hPfSIpCZc = Instance.new("TextButton", SOEJsPsYJe)
+hPfSIpCZc.Size = UDim2.new(0.8, 0, 0, 45)
+hPfSIpCZc.Position = UDim2.new(0.1, 0, 0.6, 0)
+hPfSIpCZc.Text = "LOAD SCRIPT"
+hPfSIpCZc.BackgroundColor3 = Color3.fromRGB(246, 226, 87)
+Instance.new("UICorner", hPfSIpCZc)
+hPfSIpCZc.MouseButton1Click:Connect(function()
+    uynKgbY:Destroy()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/sc/refs/heads/main/blindshot"))()
+end)
