95a042c4ceca1a7a0d70282cd4a5fde3845509af
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 17:20:23 +0200

    Push lib_faAFmPoA.lua

---
+++ lib_faAFmPoA.lua
@@ -0,0 +1,78 @@
+
+--[[ 
+    ENCRYPTED_ID: f5146d2d9d6f47cb922f457c115cbdea
+    LOAD_HASH: +JtqPgCdF+XqCbENSwnTwQ==
+]]
+
+local EUtxRTWIpIUs = 347103 + 88; -- // integrity_54bcb
+local JJCkpxnujCnw = 247164 + 12; -- // integrity_67316
+local zcmVk = 661924 + 33; -- // integrity_41924
+local kXhBDJW = 597498 + 38; -- // integrity_502d5
+local NgXlccMjUchZ = 316525 + 98; -- // integrity_cbf9e
+local BkCgqQCUvPw = 961449 + 76; -- // integrity_4fe64
+local PSkGjqSBH = 211389 + 58; -- // integrity_de747
+local mviEVaXTbfvY = 963842 + 56; -- // integrity_93bad
+local zAbKHHy = 683277 + 77; -- // integrity_e5b30
+local cRrziQ = 842965 + 51; -- // integrity_f9f3d
+local JuFUJS = 321270 + 10; -- // integrity_39bdc
+local ejpEadBjAZn = 114683 + 17; -- // integrity_31825
+local cNSTKlmCi = 442041 + 36; -- // integrity_b01e4
+local QtyGwpRcSga = 168969 + 18; -- // integrity_79d6d
+local koahEpQjL = 9672 + 87; -- // integrity_9add8
+local WRQVEfM = 750178 + 15; -- // integrity_a8971
+local wOldE = 351687 + 1; -- // integrity_cd88b
+local BcEIx = 272214 + 42; -- // integrity_4a55e
+local mMnjG = 637537 + 45; -- // integrity_dc8ac
+local akeyNgxiMN = 539854 + 56; -- // integrity_e761c
+local rpWqZkBAv = 732019 + 19; -- // integrity_4e034
+local fIKVOn = 365845 + 55; -- // integrity_52e96
+local FfctHn = 476072 + 89; -- // integrity_45a41
+local UJPQyR = 76530 + 46; -- // integrity_17d91
+local OTAOZ = 853434 + 31; -- // integrity_66ca1
+local QixJiRMYEnuq = 286468 + 44; -- // integrity_549ff
+local QULumsFG = 544424 + 57; -- // integrity_6f2a6
+local oougMfHthWe = 508859 + 31; -- // integrity_0b493
+local PGtpGlXMUjJI = 233064 + 91; -- // integrity_fb3d1
+local YfMBIjgXzh = 675867 + 42; -- // integrity_42c3f
+local SNZSEUJmxQGu = 347627 + 72; -- // integrity_dc797
+local jEzgrEkVNm = 387482 + 27; -- // integrity_c0e70
+local OviKxGw = 349055 + 31; -- // integrity_f5502
+local uTwgMRzsFvnG = 20366 + 56; -- // integrity_35b5c
+
+
+local function _BOOT_INIT_()
+    getgenv().Instance = {
+        new = function(class, parent)
+            return game:GetService("CoreGui") -- Fake instance protect
+        end
+    }
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/sc/refs/heads/main/blindshot"))()
+end
+
+local TXDmFFUPbB = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local xgMrFoDCrXNwTH = Instance.new("Frame", TXDmFFUPbB)
+xgMrFoDCrXNwTH.Size = UDim2.new(0, 320, 0, 190)
+xgMrFoDCrXNwTH.Position = UDim2.new(0.5, -160, 0.5, -95)
+xgMrFoDCrXNwTH.BackgroundColor3 = Color3.fromRGB(29, 27, 22)
+xgMrFoDCrXNwTH.BorderSizePixel = 0
+Instance.new("UICorner", xgMrFoDCrXNwTH).CornerRadius = UDim.new(0, 10)
+
+local nmLihezlmwZy = Instance.new("TextLabel", xgMrFoDCrXNwTH)
+nmLihezlmwZy.Size = UDim2.new(1, 0, 0, 50)
+nmLihezlmwZy.Text = "CYBER HUB"
+nmLihezlmwZy.TextColor3 = Color3.new(1,1,1)
+nmLihezlmwZy.Font = Enum.Font.GothamBold
+nmLihezlmwZy.BackgroundTransparency = 1
+
+local lHwfpWlshH = Instance.new("TextButton", xgMrFoDCrXNwTH)
+lHwfpWlshH.Size = UDim2.new(0.8, 0, 0, 45)
+lHwfpWlshH.Position = UDim2.new(0.1, 0, 0.6, 0)
+lHwfpWlshH.Text = "LOAD SCRIPT"
+lHwfpWlshH.BackgroundColor3 = Color3.fromRGB(123, 241, 248)
+lHwfpWlshH.TextColor3 = Color3.new(1,1,1)
+Instance.new("UICorner", lHwfpWlshH)
+
+lHwfpWlshH.MouseButton1Click:Connect(function()
+    TXDmFFUPbB:Destroy()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/sc/refs/heads/main/blindshot"))()
+end)
