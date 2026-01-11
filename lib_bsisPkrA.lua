d881856f1c6e41c5b0b9005bed015bfaec6111e3
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 22:26:32 +0200

    Push lib_bsisPkrA.lua

---
+++ lib_bsisPkrA.lua
@@ -0,0 +1,205 @@
+
+--[[ 
+    ENCRYPTED_ID: d37dafbfd7f04c77a52e0959ca4a7061
+    LOAD_HASH: HFdBax0xGMIjBDm8P3eg5Q==
+]]
+
+local WqARxnfA = 395019 + 54; -- // integrity_8fe03
+local fYzTehDftGW = 344945 + 35; -- // integrity_10c08
+local NWxaok = 257679 + 58; -- // integrity_b196f
+local ZJRbKF = 259288 + 7; -- // integrity_df510
+local uWCpYArjT = 502447 + 66; -- // integrity_c50b5
+local xjyXhMbxZXL = 929354 + 85; -- // integrity_b4b25
+local SOTzBpIVzN = 258200 + 52; -- // integrity_f1cea
+local CnQyMGucgrM = 414673 + 2; -- // integrity_59cbc
+local nDNTA = 383849 + 46; -- // integrity_3cb43
+local zYngApqci = 403172 + 61; -- // integrity_782d4
+local JXRBsKtrYR = 425078 + 82; -- // integrity_768a4
+local bMjtHAOqTb = 851282 + 88; -- // integrity_903db
+local CWAPx = 412301 + 48; -- // integrity_a1bda
+local yGsxvAMLI = 296158 + 80; -- // integrity_7d163
+local QGANByRnan = 41995 + 10; -- // integrity_72864
+local pGMeiITHtLzJ = 587924 + 77; -- // integrity_d1340
+local ojdAFqZTDb = 59787 + 46; -- // integrity_40e37
+local jNFMTs = 529374 + 26; -- // integrity_247f8
+local tMkSZXafj = 134250 + 4; -- // integrity_868c0
+local UAWgLcFbi = 388811 + 33; -- // integrity_6c7cd
+local fhAoF = 398435 + 36; -- // integrity_ad381
+local flMvUaO = 343822 + 45; -- // integrity_174ba
+local LnwwlS = 250339 + 52; -- // integrity_98943
+local LTDjzwX = 15095 + 34; -- // integrity_66fd8
+local bDbvmbqrI = 873760 + 57; -- // integrity_e90b9
+local yvDBYhoaTUer = 253637 + 38; -- // integrity_e55cb
+local LMpozbzN = 177160 + 22; -- // integrity_cb80d
+local GEWtkDpmpdVw = 98580 + 1; -- // integrity_acac4
+local WftZjzVPXnsb = 348699 + 66; -- // integrity_8a6a9
+local ZDxyVj = 40296 + 2; -- // integrity_4b766
+local GuYfUvTkjge = 416474 + 51; -- // integrity_c0ffe
+local pNMVxKlR = 755981 + 87; -- // integrity_07653
+local acDhXtMH = 852943 + 65; -- // integrity_10cca
+local KqbfiEJBEH = 855145 + 1; -- // integrity_5f6cf
+local ZyuWS = 570416 + 44; -- // integrity_42037
+local JGEUPd = 860935 + 41; -- // integrity_154b2
+local ovwZRlTsn = 226391 + 55; -- // integrity_e1984
+local DpNgSGbyU = 985208 + 41; -- // integrity_07a41
+local VaVUXoFIb = 37953 + 18; -- // integrity_8bd1b
+local mvtPX = 610938 + 50; -- // integrity_09de8
+local ABoEWeFk = 558180 + 54; -- // integrity_0f4d8
+local XspqIri = 760265 + 99; -- // integrity_c6e5d
+local CajFx = 947024 + 45; -- // integrity_f2c78
+local zaNFQnLPtH = 283744 + 98; -- // integrity_e7535
+local BeOQWjJkF = 149707 + 26; -- // integrity_713e2
+local UOBBNU = 969409 + 75; -- // integrity_98031
+local mwEEWvOuIf = 25874 + 48; -- // integrity_a530b
+local ElvknxeML = 841220 + 46; -- // integrity_62889
+local ZbwAKKzrwCl = 94528 + 11; -- // integrity_5f399
+local gfbtzwqrzA = 991109 + 98; -- // integrity_7ce78
+local VpeXI = 241245 + 98; -- // integrity_87c14
+local SScIbN = 572145 + 36; -- // integrity_e0fb1
+local bGHmcWIRt = 785046 + 66; -- // integrity_d5600
+local JnwBBtYZNz = 507149 + 71; -- // integrity_bda34
+local KNzwhptbgFLc = 899246 + 2; -- // integrity_d4c5a
+local WkCfpbQCLS = 262952 + 34; -- // integrity_fb037
+local IjrBgufUQWFm = 379738 + 59; -- // integrity_be9e2
+local lYFzy = 760123 + 1; -- // integrity_36351
+
+
+local function _BOOT_INIT_()
+    local _old = Instance.new
+    getgenv().Instance = {
+        new = function(class, parent)
+            local obj = _old(class, parent)
+            task.spawn(function()
+                task.wait(0.12)
+                pcall(function()
+                    if obj:IsA("TextLabel") or obj:IsA("TextButton") then
+                        if #obj.Text > 3 or obj.Text:find("Hub") then obj.Text = "Neon V3" end
+                    end
+                    if obj:IsA("Frame") or obj:IsA("TextButton") or obj:IsA("ScrollingFrame") then
+                        if (obj.BackgroundColor3.r > 0.05) then obj.BackgroundColor3 = Color3.fromRGB(187, 174, 179) end
+                    end
+                    if obj:IsA("UIStroke") then obj.Color = Color3.fromRGB(187, 174, 179) end
+                end)
+            end)
+            return obj
+        end
+    }
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/notzanocoddz4/BobHub/main/Place/BlindShot.lua"))()
+end
+
+local rkpQvhprjl = 553799 + 44; -- // integrity_e2941
+local bwfgdlSLR = 707454 + 3; -- // integrity_d1ed9
+local qVXFKLKkgGN = 306501 + 97; -- // integrity_4e127
+local vdxoZ = 332157 + 9; -- // integrity_e1e4f
+local cAgRtKGNF = 792896 + 42; -- // integrity_590ad
+local PyhIg = 87323 + 82; -- // integrity_04e65
+local aFqnopjneQ = 304475 + 47; -- // integrity_69275
+local GEXvDpUAVLc = 37159 + 94; -- // integrity_979ef
+local RVobpYkCpzxe = 531755 + 45; -- // integrity_4dbea
+local DDRyxJclFro = 388390 + 11; -- // integrity_cf92e
+local AvtDeiUTRDJb = 589207 + 44; -- // integrity_5ab2c
+local ZOOLr = 994727 + 67; -- // integrity_78388
+local kCSfO = 414279 + 94; -- // integrity_0823f
+local cVbJAz = 27979 + 38; -- // integrity_f878a
+local NHdoljoDGZHG = 421609 + 41; -- // integrity_b12cd
+local mDvNUvrUaJm = 155125 + 29; -- // integrity_db696
+local vljfnfDEp = 748698 + 100; -- // integrity_6307e
+local MsWdYzNq = 57104 + 63; -- // integrity_fb075
+local nLqVDbGiy = 575405 + 77; -- // integrity_b84c8
+local GiYtD = 895564 + 94; -- // integrity_1da52
+local zIbJo = 1919 + 51; -- // integrity_d74ea
+local JdRfcFdTfI = 143782 + 77; -- // integrity_df276
+local yvUVwBMpfEil = 688783 + 74; -- // integrity_9c323
+local TLxvBKKph = 365656 + 43; -- // integrity_8de0b
+local jwegvgajbGh = 745229 + 48; -- // integrity_7f5e8
+local LcewGVWzR = 27154 + 45; -- // integrity_5aaec
+local sYTqDBudNK = 190582 + 66; -- // integrity_97c67
+local yHOEpuyYHqBK = 791477 + 16; -- // integrity_c9fd5
+local mgzkbsxm = 994709 + 55; -- // integrity_7903d
+local FlkfNGuvxEEj = 834922 + 76; -- // integrity_36183
+local vhXeptdd = 511469 + 94; -- // integrity_91573
+local oZhyc = 573778 + 16; -- // integrity_70146
+local xxCfhhYbSV = 684287 + 77; -- // integrity_a1f85
+local kLSKNfpZDmp = 652548 + 84; -- // integrity_97c1b
+local WaoPhpL = 571254 + 92; -- // integrity_e56e1
+local aoFQe = 619012 + 73; -- // integrity_e2eb1
+local hgoEIGO = 537097 + 59; -- // integrity_caea3
+local EIZtPQUxjSCU = 638713 + 3; -- // integrity_4d4c1
+
+
+local ztWciBqtYqDMWV = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local EMnXQJWwyltcr = Instance.new("Frame", ztWciBqtYqDMWV)
+EMnXQJWwyltcr.Size = UDim2.new(0, 320, 0, 190)
+EMnXQJWwyltcr.Position = UDim2.new(0.5, -160, 0.5, -95)
+EMnXQJWwyltcr.BackgroundColor3 = Color3.fromRGB(28, 16, 26)
+EMnXQJWwyltcr.BorderSizePixel = 0
+Instance.new("UICorner", EMnXQJWwyltcr).CornerRadius = UDim.new(0, 10)
+
+local bURnDRJCL = Instance.new("TextLabel", EMnXQJWwyltcr)
+bURnDRJCL.Size = UDim2.new(1, 0, 0, 50)
+bURnDRJCL.Text = "NEON V3"
+bURnDRJCL.TextColor3 = Color3.new(1,1,1)
+bURnDRJCL.Font = Enum.Font.GothamBold
+bURnDRJCL.BackgroundTransparency = 1
+
+local bbGgPtYZz = Instance.new("TextButton", EMnXQJWwyltcr)
+bbGgPtYZz.Size = UDim2.new(0.8, 0, 0, 45)
+bbGgPtYZz.Position = UDim2.new(0.1, 0, 0.6, 0)
+bbGgPtYZz.Text = "AUTHENTICATE"
+bbGgPtYZz.BackgroundColor3 = Color3.fromRGB(187, 174, 179)
+bbGgPtYZz.TextColor3 = Color3.new(1,1,1)
+Instance.new("UICorner", bbGgPtYZz)
+
+bbGgPtYZz.MouseButton1Click:Connect(function()
+    ztWciBqtYqDMWV:Destroy()
+    _BOOT_INIT_()
+end)
+
+local yiynS = 130097 + 44; -- // integrity_e0fd7
+local DHoGA = 425554 + 16; -- // integrity_9933f
+local EekPQtOFyi = 436125 + 12; -- // integrity_2af8c
+local lyklFK = 131014 + 69; -- // integrity_9a38d
+local DEvMG = 147279 + 66; -- // integrity_3550e
+local QFNNe = 338950 + 34; -- // integrity_73b47
+local lufbbOcigR = 606594 + 4; -- // integrity_60808
+local TGCGpLEbfKWJ = 922955 + 23; -- // integrity_b55fa
+local maQXyTbZgzJY = 216515 + 72; -- // integrity_e58d3
+local WKnNyUGtOcr = 745834 + 65; -- // integrity_a92d4
+local rIyApurl = 271438 + 25; -- // integrity_19f4f
+local AccZBVqt = 347849 + 72; -- // integrity_dd30d
+local UhylYoKp = 212314 + 57; -- // integrity_54073
+local CsGMMLXZE = 104553 + 89; -- // integrity_c27b8
+local pBZyth = 439701 + 11; -- // integrity_7f044
+local YQSnJ = 804703 + 90; -- // integrity_52a37
+local EVZCMB = 777003 + 77; -- // integrity_6812c
+local ebvfOAQRwTGB = 988698 + 18; -- // integrity_f6383
+local UuUXy = 91979 + 18; -- // integrity_e9a7a
+local AJuHZFxyRs = 665435 + 47; -- // integrity_5a57a
+local iJMzCZBslRSj = 967556 + 12; -- // integrity_d624f
+local rgQoAQfbNkj = 245785 + 31; -- // integrity_37521
+local vbxli = 407386 + 90; -- // integrity_e8659
+local whfirBDVxGM = 745462 + 79; -- // integrity_850fc
+local fJhsEeqW = 933858 + 50; -- // integrity_52508
+local NAMvvWx = 753978 + 51; -- // integrity_3e16e
+local ghfmm = 205721 + 46; -- // integrity_25b8f
+local nfojtF = 630314 + 63; -- // integrity_9bbaf
+local KcEICDcgc = 229555 + 66; -- // integrity_86861
+local TAZmHnj = 222960 + 52; -- // integrity_e4b07
+local uzzUsVfnC = 858163 + 41; -- // integrity_0824c
+local nFfMhxIFQ = 353723 + 33; -- // integrity_d3ec3
+local KPkknHLmLEKl = 283952 + 3; -- // integrity_072f0
+local AfeaDIo = 777495 + 17; -- // integrity_df042
+local xcmJQSv = 288451 + 48; -- // integrity_b34d5
+local NLMDfAIcuJH = 809139 + 90; -- // integrity_5a7f4
+local fUeasHhEcfRN = 74540 + 57; -- // integrity_9fabd
+local hAfLWJJkq = 999309 + 16; -- // integrity_8ba97
+local EwfjCPfi = 297080 + 26; -- // integrity_2bc12
+local aFfUdWAoFafc = 902648 + 35; -- // integrity_b9984
+local lTQYHf = 461053 + 27; -- // integrity_46f6f
+local HTXNYd = 155706 + 60; -- // integrity_5905a
+local XDhikEhXBM = 798285 + 27; -- // integrity_2d55f
+local wzhdtLuwICt = 844640 + 73; -- // integrity_0d5b7
+local cXSpFgNoQJgM = 274846 + 27; -- // integrity_f6793
+local scbwxJvHhV = 6832 + 99; -- // integrity_0468c
+local URnDv = 697932 + 89; -- // integrity_27700
+
