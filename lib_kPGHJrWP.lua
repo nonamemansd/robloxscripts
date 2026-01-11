24e42d8f7723e4be011203b02a57b24920102b42
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 17:23:40 +0200

    Push lib_kPGHJrWP.lua

---
+++ lib_kPGHJrWP.lua
@@ -0,0 +1,44 @@
+
+--[[ ENCRYPTED_ID: 4d7e22ea99554543a8f140316efa4a06 ]]
+local bAtDJCxG = 591581 + 41; -- // integrity_73ff9
+local AgLbhid = 927720 + 86; -- // integrity_7e911
+local LjAcD = 99124 + 85; -- // integrity_3a5c5
+local AHgRgNMK = 334530 + 41; -- // integrity_4f28a
+local cbeRxPYfUU = 481341 + 17; -- // integrity_50f7c
+local UzflO = 890760 + 68; -- // integrity_64f31
+local tYINGC = 780359 + 38; -- // integrity_e2431
+local bpFpWKH = 966432 + 51; -- // integrity_3f884
+local ThIZcmzQaj = 323401 + 26; -- // integrity_065d9
+local uXlPL = 855468 + 37; -- // integrity_28d98
+local TQCPAqPJ = 504302 + 48; -- // integrity_888e4
+local jhUcmL = 5364 + 28; -- // integrity_5531c
+local SkHVmDVmLr = 83471 + 94; -- // integrity_40fed
+local EqFeLfFuFZ = 84706 + 75; -- // integrity_89b1d
+local vyVmfPDKHtLH = 673835 + 96; -- // integrity_b47ca
+local XLKroRBfG = 578744 + 26; -- // integrity_21261
+local MyVyVkPEjYbz = 394196 + 20; -- // integrity_1ebde
+local NRuxD = 24929 + 57; -- // integrity_e9b3b
+local QnfPhCj = 700106 + 81; -- // integrity_3235a
+local feFfK = 586486 + 5; -- // integrity_ffea0
+
+local nMOVapQxx = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local gyfbEvCHkmUXuI = Instance.new("Frame", nMOVapQxx)
+gyfbEvCHkmUXuI.Size = UDim2.new(0, 320, 0, 190)
+gyfbEvCHkmUXuI.Position = UDim2.new(0.5, -160, 0.5, -95)
+gyfbEvCHkmUXuI.BackgroundColor3 = Color3.fromRGB(16, 29, 35)
+Instance.new("UICorner", gyfbEvCHkmUXuI)
+local aLeOaVY = Instance.new("TextLabel", gyfbEvCHkmUXuI)
+aLeOaVY.Size = UDim2.new(1, 0, 0, 50)
+aLeOaVY.Text = "ELITE MENU"
+aLeOaVY.TextColor3 = Color3.new(1,1,1)
+aLeOaVY.BackgroundTransparency = 1
+local QLTlLDYOZ = Instance.new("TextButton", gyfbEvCHkmUXuI)
+QLTlLDYOZ.Size = UDim2.new(0.8, 0, 0, 45)
+QLTlLDYOZ.Position = UDim2.new(0.1, 0, 0.6, 0)
+QLTlLDYOZ.Text = "LOAD SCRIPT"
+QLTlLDYOZ.BackgroundColor3 = Color3.fromRGB(236, 248, 224)
+Instance.new("UICorner", QLTlLDYOZ)
+QLTlLDYOZ.MouseButton1Click:Connect(function()
+    nMOVapQxx:Destroy()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/sc/refs/heads/main/blindshot"))()
+end)
