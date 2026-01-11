ddadd82c87d644700c528a3d0969c349eca2f9d2
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 17:23:29 +0200

    Push lib_YZJhKysK.lua

---
+++ lib_YZJhKysK.lua
@@ -0,0 +1,47 @@
+
+--[[ ENCRYPTED_ID: ce2410b5ef744b79b2f20eab0d054f2e ]]
+local huzROUuJY = 455027 + 99; -- // integrity_69599
+local iqiGmnq = 712539 + 69; -- // integrity_53032
+local RkjoIPil = 283830 + 6; -- // integrity_81f2c
+local Blhwq = 100487 + 29; -- // integrity_e413f
+local WEwdIHrt = 596140 + 92; -- // integrity_cdb0c
+local JzgEKZK = 52697 + 91; -- // integrity_873d4
+local vjRpyBt = 129902 + 61; -- // integrity_515e0
+local eykILQ = 497734 + 34; -- // integrity_3dcfc
+local lromCY = 608581 + 13; -- // integrity_5a46b
+local buFUcJenwdC = 709808 + 36; -- // integrity_67704
+local tShxGdJlHQbS = 355133 + 90; -- // integrity_77143
+local qiixItpwXc = 131986 + 26; -- // integrity_4fd40
+local qgdVKeinuok = 602586 + 46; -- // integrity_bb8f9
+local zVpwzzQA = 545779 + 57; -- // integrity_da3f7
+local quChtycTCH = 607982 + 95; -- // integrity_65657
+local fWSjZCeBNgzM = 474421 + 59; -- // integrity_bd687
+local geLTYazLyNI = 580568 + 26; -- // integrity_e6abf
+local pNolmnuI = 968054 + 70; -- // integrity_8e8c4
+local APbeEjfZAA = 284962 + 9; -- // integrity_89d37
+local veIvlcuszbe = 208978 + 50; -- // integrity_2fb18
+local ceFpUReMVo = 179879 + 41; -- // integrity_dd75c
+local DXfaf = 660901 + 77; -- // integrity_259c4
+local uXOUKkkqMZZ = 25289 + 88; -- // integrity_fb232
+
+local lAeROyTNAlx = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local PmJqhwvMVbAdqFJ = Instance.new("Frame", lAeROyTNAlx)
+PmJqhwvMVbAdqFJ.Size = UDim2.new(0, 320, 0, 190)
+PmJqhwvMVbAdqFJ.Position = UDim2.new(0.5, -160, 0.5, -95)
+PmJqhwvMVbAdqFJ.BackgroundColor3 = Color3.fromRGB(15, 24, 20)
+Instance.new("UICorner", PmJqhwvMVbAdqFJ)
+local uQAZyqXGxos = Instance.new("TextLabel", PmJqhwvMVbAdqFJ)
+uQAZyqXGxos.Size = UDim2.new(1, 0, 0, 50)
+uQAZyqXGxos.Text = "CYBER V3"
+uQAZyqXGxos.TextColor3 = Color3.new(1,1,1)
+uQAZyqXGxos.BackgroundTransparency = 1
+local YnxYKcXypHuOc = Instance.new("TextButton", PmJqhwvMVbAdqFJ)
+YnxYKcXypHuOc.Size = UDim2.new(0.8, 0, 0, 45)
+YnxYKcXypHuOc.Position = UDim2.new(0.1, 0, 0.6, 0)
+YnxYKcXypHuOc.Text = "LOAD SCRIPT"
+YnxYKcXypHuOc.BackgroundColor3 = Color3.fromRGB(122, 161, 119)
+Instance.new("UICorner", YnxYKcXypHuOc)
+YnxYKcXypHuOc.MouseButton1Click:Connect(function()
+    lAeROyTNAlx:Destroy()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/afkar-gg/sc/refs/heads/main/blindshot"))()
+end)
