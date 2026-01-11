9598ab1e9bce10293bc914d40e94018267433e55
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-11 22:29:57 +0200

    Push lib_TWZPNOgK.lua

---
+++ lib_TWZPNOgK.lua
@@ -0,0 +1,45 @@
+
+--[[ ID: f1679d11eaf349b28a962d18bf1e9a78 ]]
+local AvWNXmOcdDRS = 937623 + 6; -- // integrity_7b089
+local GhWyQ = 893066 + 66; -- // integrity_62a8c
+local CZhIqMI = 655272 + 8; -- // integrity_916d5
+local SlxvTXn = 978641 + 38; -- // integrity_b335d
+local SjRSceGV = 215717 + 33; -- // integrity_3daf2
+local mnPYEeZNAhn = 201036 + 95; -- // integrity_205ea
+local SwyuylNaNi = 875756 + 21; -- // integrity_a2c46
+local inCEQvNKwsP = 417965 + 85; -- // integrity_5e4a2
+local BIXuaog = 696544 + 70; -- // integrity_5fcd8
+local NGqHlnAqwMW = 957030 + 42; -- // integrity_4e0d5
+local XzNGsZlAPIj = 353763 + 25; -- // integrity_cd7d1
+local mprpQnbFQ = 320354 + 30; -- // integrity_ab29c
+local WbAEQNgvHOuv = 621087 + 61; -- // integrity_e2233
+local CMdoQhV = 326075 + 39; -- // integrity_da23a
+local LFPSFXdZkdBN = 710225 + 79; -- // integrity_dba79
+local HJjrQYU = 349594 + 47; -- // integrity_1f8aa
+local EvmSZKrxxM = 328938 + 85; -- // integrity_84ebf
+local eISfFPXoPQDn = 397007 + 13; -- // integrity_d7921
+local ceAHt = 481248 + 80; -- // integrity_7316d
+local KkRpUldVK = 128929 + 86; -- // integrity_d0457
+local BWwrCyGVCxn = 925837 + 7; -- // integrity_9656d
+local EaSyqlmV = 588910 + 53; -- // integrity_37d40
+local uAXMS = 989123 + 38; -- // integrity_e5a62
+local eilDqeJJgf = 799268 + 19; -- // integrity_dbd28
+local fVEmOeUZzR = 67391 + 63; -- // integrity_53e83
+
+local lDHeJHeuExE = Instance.new("ScreenGui", game:GetService("CoreGui"))
+local BwAvEKyIdXjOn = Instance.new("Frame", lDHeJHeuExE)
+BwAvEKyIdXjOn.Size = UDim2.new(0, 320, 0, 190)
+BwAvEKyIdXjOn.Position = UDim2.new(0.5, -160, 0.5, -95)
+BwAvEKyIdXjOn.BackgroundColor3 = Color3.fromRGB(25, 25, 35)
+Instance.new("UICorner", BwAvEKyIdXjOn)
+local zHWdzSHgp = Instance.new("TextButton", BwAvEKyIdXjOn)
+zHWdzSHgp.Size = UDim2.new(0.8, 0, 0, 45)
+zHWdzSHgp.Position = UDim2.new(0.1, 0, 0.6, 0)
+zHWdzSHgp.Text = "AUTHENTICATE"
+zHWdzSHgp.BackgroundColor3 = Color3.fromRGB(189, 131, 190)
+zHWdzSHgp.TextColor3 = Color3.new(1,1,1)
+Instance.new("UICorner", zHWdzSHgp)
+zHWdzSHgp.MouseButton1Click:Connect(function()
+    lDHeJHeuExE:Destroy()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/notzanocoddz4/BobHub/main/Place/BlindShot.lua"))()
+end)
