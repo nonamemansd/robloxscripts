dda55732630ffeed10b8532420b6ea5b802b89b3
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-29 20:28:42 +0200

    Auto-upload PL_4XXoCncm.lua

---
+++ PL_4XXoCncm.lua
@@ -0,0 +1,39 @@
+
+--[[ STAGE 2: VISUAL CORE ]]
+local HubName = "MOKROA"
+local Acc = Color3.fromRGB(0, 255, 120)
+
+-- // SMART HOOK //
+local function Hook()
+    local function Customize(obj)
+        pcall(function()
+            -- 1. ЗАГОЛОВКИ (Smart Filter)
+            if obj:IsA("TextLabel") then
+                if obj.TextSize >= 18 or obj.Name:find("Title") or obj.Name:find("Header") then
+                    if #obj.Text > 3 and not tonumber(obj.Text) then
+                        obj.Text = HubName
+                        obj.TextColor3 = Acc
+                    end
+                end
+            end
+            
+            -- 2. СКРУГЛЕНИЯ (Modern UI)
+            if obj:IsA("Frame") or obj:IsA("TextButton") then
+                if obj.BackgroundColor3.R < 0.2 then
+                    if not obj:FindFirstChild("UICorner") then
+                        local uic = Instance.new("UICorner")
+                        uic.CornerRadius = UDim.new(0, 6)
+                        uic.Parent = obj
+                    end
+                end
+            end
+        end)
+    end
+    
+    game.DescendantAdded:Connect(function(c)
+        if c:IsA("GuiObject") then task.delay(0.1, function() Customize(c) end) end
+    end)
+end
+
+Hook()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 69, 100, 103, 101, 73, 89, 47, 105, 110, 102, 105, 110, 105, 116, 101, 121, 105, 101, 108, 100, 47, 109, 97, 115, 116, 101, 114, 47, 115, 111, 117, 114, 99, 101)))()
